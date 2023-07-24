b2a4412798089192bd05299a8280563e76cf86ff
SalehinShohan <ps.shohan1@gmail.com>
2023-07-24 22:14:18 +0600

    add some updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/routes.jsx'
-import AuthProvider from './Context/AuthProvider'
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/routes.jsx";
+import AuthProvider from "./Context/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="">
+    <React.StrictMode>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </React.StrictMode>
+  </div>
+);
